8d185518a0ea8bddf5d87dbd8f0663c9cfe7bcb5
SXYXH <bacxr1945@gmail.com>
2024-06-13 17:23:51 +0900

    d

--- src/main/java/ksmart/springboot/member/controller/MemberController.java
+++ src/main/java/ksmart/springboot/member/controller/MemberController.java
@@ -2,11 +2,10 @@ package ksmart.springboot.member.controller;
 
 import ksmart.springboot.exam.dto.Member;
 import ksmart.springboot.exam.service.ExamService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -22,6 +21,20 @@ public class MemberController {
         return examService.getMemberList();
     }
 
+    /**
+     * @RequestBody 요청한 json 객체를 서버에서 바인딩 받을 때 사용하는 어노테이션
+     * json 전달되는 데이터 타입에 맞게 자바타입을 선언해야한다
+     * DTO로 선언하고 데이터바인딩 받는게 유지보수시 유리
+     * @param paramMap
+     * @return
+     */
+    @PostMapping("/member/searchId")
+    public Member getMemberInfo(@RequestBody Map<String,String> paramMap) {
+        String memberId  = paramMap.get("memberId");
+        //System.out.println("Member : " + member);
+        System.out.println("MemberId : " + memberId);
+        return examService.getMemberbyId(memberId);
+    }
     /**
      *    @PathVariable : 주소 맵핑시 { 바인딩 받을 키 }
      *    핸들러 메소드에 매개변수의 @PathVariable(name="바인딩 받을 키")  데이터타입 변수명
